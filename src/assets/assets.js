76b925ec19c07a2609fc8292fd5660ffd887e427
Niyati <agrawalniyati20032gmail.com>
2025-07-25 11:46:20 +0530

    Initial project setup

---
+++ src/assets/assets.js
@@ -0,0 +1,103 @@
+import logo from './logo.png'
+import crossicon from './crossicon.png'
+import menuicon from './menuicon.png'
+import staricon from './staricon.png'
+import leftarrow from './leftarrow.png'
+import rightarrow from './rightarrow.png'
+import header from './header.png'
+import brand from './brand.png'
+import project1 from './project1.jpg'
+import project2 from './project2.jpg'
+import project3 from './project3.jpg'
+import project4 from './project4.jpg'
+import project5 from './project5.jpg'
+import project6 from './project6.jpg'
+import profile1 from './profile1.png'
+import profile2 from './profile2.png'
+import profile3 from './profile3.png'
+
+export const assets ={
+    logo,
+    crossicon,
+    menuicon,
+    staricon,
+    leftarrow,
+    rightarrow,
+    header,
+    brand,
+    project1,
+    project2,
+    project3,
+    project4,
+    project5,
+    project6,
+    profile1,
+    profile2,
+    profile3,
+};
+
+export const projectsData = [
+    {
+        tittle: "Skyline Haven",
+        price: "$2,50,000",
+        location: "Califonia",
+        Image:project1,
+    },
+    {
+        tittle: "Vista Verde",
+        price: "$2,50,000",
+        location: "San Francisco ",
+        Image :project2,
+    },
+    {
+        tittle: "Serenity Suites",
+        price: "$2,50,000",
+        location: "Chicago",
+        Image :project3,
+    },
+    {
+        tittle: "Central Square",
+        price: "$2,50,000",
+        location: "Califonia",
+        Image :project4,
+    },
+    {
+        tittle: "Vista Verde",
+        price: "$2,50,000",
+        location: "San Francisco",
+        Image :project5,
+    },
+    {
+        tittle: "Serenity Suites",
+        price: "$2,50,000",
+        location: "Chicago",
+        Image :project6,
+    },
+];
+
+export const testimonialsData = [
+    {
+        name : "Donald Jackman",
+        tittle: "Marketing Manager",
+        Image : profile1,
+        alt :"Portrait of Donald Jackman",
+        rating: 5,
+        text : "From the first meeting to final handover, the process was smooth and stress-free. The team truly understood our vision and made it a reality. We couldn’t be happier with our new home"
+    },
+    {
+        name : "Rechard Nelson",
+        tittle: "UI/UX Designer",
+        Image : profile2,
+        alt :"Portrait of Donald Jackman",
+        rating: 4,
+        text : "They go above and beyond. The construction quality, the finishes, the after-sales service — everything was impressive. It’s rare to find this level of commitment today"
+    },
+    {
+        name : "James Washington",
+        tittle: "Co-Founder",
+        Image : profile3,
+        alt :"Portrait of Donald Jackman",
+        rating: 5,
+        text : "What stood out most was their honesty and transparency. No hidden costs, no delays. The site updates and regular communication gave us full confidence in the process."
+    },
+];
